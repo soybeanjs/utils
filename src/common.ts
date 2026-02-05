14b11c4662605ab80b45c09491dd5085bea50226
Soybean <soybeanjs@outlook.com>
2026-02-05 11:20:26 +0800

    feat(common): add isNullish utility function to check for null or undefined values

--- src/common.ts
+++ src/common.ts
@@ -4,3 +4,5 @@ export const entriesOf = <T extends Record<string, unknown>>(record: T) =>
   Object.entries(record) as Array<[keyof T, T[keyof T]]>;
 
 export const valuesOf = <T extends Record<string, unknown>>(record: T) => Object.values(record) as T[keyof T][];
+
+export const isNullish = (value: unknown): value is null | undefined => value === null || value === undefined;
